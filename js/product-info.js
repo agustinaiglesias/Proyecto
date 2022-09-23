9d35741912016a0ce31f7f3225545685d84040e6
Agustina Iglesias <110361516+agustinaiglesias@users.noreply.github.com>
2022-09-23 10:44:37 -0300

    entrega 4

--- js/product-info.js
+++ js/product-info.js
@@ -12,6 +12,7 @@ function showProduct(producto) {
             
             <div class="at-container">
                 <div class="atributos-cont">
+                <h1>${producto.name} </h1>
                     <p class="atributosProd"> <b>Precio:</b> $${producto.cost}</p>
                     <p class="atributosProd"><b>Descripción:</b> ${producto.description}</p>
                     <p class="atributosProd"><b>Categoría:</b> ${producto.category}</p>
@@ -21,12 +22,12 @@ function showProduct(producto) {
             `
 
     document.getElementById("descripcion-container").innerHTML = htmlContentToAppend;
-    htmlContentToAppend = "";
+    /*htmlContentToAppend = "";
     htmlContentToAppend += `
             
             <h1>${producto.name} </h1>
             `
-    document.getElementById("nombreProducto").innerHTML = htmlContentToAppend;
+    document.getElementById("nombreProducto").innerHTML = htmlContentToAppend;*/
     
 };
 
@@ -64,7 +65,7 @@ function showProductComments(comentariosArray) {
 
 
 
-function showImg(imgArray) {
+/*function showImg(imgArray) {
     let htmlContentToAppend = "";
     for (let i = 0; i < imgArray.length; i++) {
         let imagen = imgArray[i];
@@ -75,13 +76,64 @@ function showImg(imgArray) {
                 <div class="foto">
                 <img src="${imagen}" alt=""></img>                
                 </div>
+
+                
                 `
 
         document.getElementById("galeria").innerHTML = htmlContentToAppend;
     };
     
 
-}
+}*/
+
+function showImg(imgArray) {
+    let imagen1 = imgArray[0];
+    let imagen2 = imgArray[1];
+    let imagen3 = imgArray[2];
+    let imagen4 = imgArray[3];
+    let htmlContentToAppend = "";
+    
+        htmlContentToAppend += `
+                
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="${imagen1}" class="d-block w-100" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="${imagen2}" class="d-block w-100" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="${imagen3}" class="d-block w-100" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="${imagen4}" class="d-block w-100" alt="">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+                
+                `
+
+        document.getElementById("galeria").innerHTML = htmlContentToAppend;
+    };
+    
+
+
 
 function comentar(){
         let comentario = document.getElementById("comentarioIn").value;
@@ -113,6 +165,32 @@ function comentar(){
             
 }
 
+function showRecomendados(producto){
+    let htmlContentToAppend = "";
+    let recomendados = producto.relatedProducts;
+        htmlContentToAppend += `
+                
+       
+        <div class="card">
+            <img onclick="redireccionar(${recomendados[0].id})" src="${recomendados[0].image}">
+            <h4>${recomendados[0].name}</h4>
+        </div>
+        
+        <div class="card">
+            <img onclick="redireccionar(${recomendados[0].id})" src="${recomendados[1].image}">
+            <h4>${recomendados[0].name}</h4>
+        </div>
+        
+        `
+
+        document.getElementById("recomendados").innerHTML += htmlContentToAppend;
+}
+
+function redireccionar(id) {
+    localStorage.setItem("prodID", id);
+    window.location = "product-info.html"
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
     getJSONData(PRODUCT_INFO_URL + productID + EXT_TYPE).then(function (resultObj) {
@@ -120,6 +198,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
             currentProduct = resultObj.data
             showProduct(currentProduct);
             showImg(currentProduct.images);
+            showRecomendados(currentProduct);
         }
     });
 
